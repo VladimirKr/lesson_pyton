50bb44217dbce03812844100bd3a429841e28c93
VladimirKr <red33@mail.ru>
2022-01-27 18:48:57 +0300

    lesson 2.py

--- lesson 2.py
+++ lesson 2.py
@@ -79,19 +79,19 @@
 # Пользователь ввёл число 1. Результат: 7, 5, 3, 3, 2, 1.
 # Набор натуральных чисел можно задать сразу в коде, например, my_list = [7, 5, 3, 3, 2].
 
-my_list = [7, 5, 3, 3, 2]
-print(f"Рейтинг - {my_list}")
-number = int(input("Введите число (для выхода введите: 0): "))
-while number != 0:
-    for el in range(len(my_list)):
-        if my_list[el] == number:
-            my_list.insert(el + 1, number)
-            break
-        elif my_list[0] < number:
-            my_list.insert(0, number)
-        elif my_list[-1] > number:
-            my_list.append(number)
-        elif my_list[el] > number and my_list[el + 1] < number:
-            my_list.insert(el + 1, number)
-    print(f"текущий список - {my_list}")
-    number = int(input("Введите число: "))
+# my_list = [7, 5, 3, 3, 2]
+# print(f"Рейтинг - {my_list}")
+# number = int(input("Введите число (для выхода введите: 0): "))
+# while number != 0:
+#     for el in range(len(my_list)):
+#         if my_list[el] == number:
+#             my_list.insert(el + 1, number)
+#             break
+#         elif my_list[0] < number:
+#             my_list.insert(0, number)
+#         elif my_list[-1] > number:
+#             my_list.append(number)
+#         elif my_list[el] > number and my_list[el + 1] < number:
+#             my_list.insert(el + 1, number)
+#     print(f"текущий список - {my_list}")
+#     number = int(input("Введите число: "))
